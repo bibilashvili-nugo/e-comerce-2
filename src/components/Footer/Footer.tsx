a6246c4755b6f3c074446537a0930202942c2bd3
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-06-14 11:30:38 -0700

    footer
    footer update add media

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./footer.module.css";
+import Logo from "../../../public/shared/audiophile.png";
 import Facebook from "../../../public/social/Facebook.png";
 import Instagram from "../../../public/social/Instagram.png";
 import Linkedin from "../../../public/social/linkedin.png";
@@ -8,9 +9,9 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={`container ${styles.footerContainer}`}>
-        <a href="#" className="footer-logo">
-          audiophile
-        </a>
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
         <nav className={styles.footerLinks}>
           <Link to="/">HOME</Link>
           <Link to="/products/headphones">HEADPHONES</Link>
@@ -26,52 +27,69 @@ const Footer = () => {
             to helping you get the most out of personal audio. Come and visit
             our demo facility - we’re open 7 days a week.
           </p>
+          <div className={styles.socialAndCopy}>
+            <p>Copyright 2021. All Rights Reserved</p>
 
-          <p>Copyright 2021. All Rights Reserved</p>
-        </div>
-        <div className={styles.footerSocial}>
-          <Link to="https://www.facebook.com/bibilashvili.nugo" target="_blank">
-            <button
-              style={{ border: "none", background: "none", cursor: "pointer" }}
-              className={styles.linkButton}
-            >
-              <img
-                src={Facebook}
-                alt="Facebook"
-                style={{ width: "25px", height: "25px" }}
-              />
-            </button>
-          </Link>
-          <Link
-            to="https://www.linkedin.com/in/nugzar-bibilashvili-93b84629a/"
-            target="_blank"
-          >
-            <button
-              style={{ border: "none", background: "none", cursor: "pointer" }}
-              className={styles.linkButton}
-            >
-              <img
-                src={Linkedin}
-                alt="Linkedin"
-                style={{ width: "25px", height: "25px" }}
-              />
-            </button>
-          </Link>
-          <Link
-            to="https://www.instagram.com/bibilashvili_nugo/"
-            target="_blank"
-          >
-            <button
-              style={{ border: "none", background: "none", cursor: "pointer" }}
-              className={styles.linkButton}
-            >
-              <img
-                src={Instagram}
-                alt="Instagram"
-                style={{ width: "25px", height: "25px" }}
-              />
-            </button>
-          </Link>
+            <div className={styles.footerSocial}>
+              <Link
+                to="https://www.facebook.com/bibilashvili.nugo"
+                target="_blank"
+              >
+                <button
+                  style={{
+                    border: "none",
+                    background: "none",
+                    cursor: "pointer",
+                  }}
+                  className={styles.linkButton}
+                >
+                  <img
+                    src={Facebook}
+                    alt="Facebook"
+                    style={{ width: "25px", height: "25px" }}
+                  />
+                </button>
+              </Link>
+              <Link
+                to="https://www.linkedin.com/in/nugzar-bibilashvili-93b84629a/"
+                target="_blank"
+              >
+                <button
+                  style={{
+                    border: "none",
+                    background: "none",
+                    cursor: "pointer",
+                  }}
+                  className={styles.linkButton}
+                >
+                  <img
+                    src={Linkedin}
+                    alt="Linkedin"
+                    style={{ width: "25px", height: "25px" }}
+                  />
+                </button>
+              </Link>
+              <Link
+                to="https://www.instagram.com/bibilashvili_nugo/"
+                target="_blank"
+              >
+                <button
+                  style={{
+                    border: "none",
+                    background: "none",
+                    cursor: "pointer",
+                  }}
+                  className={styles.linkButton}
+                >
+                  <img
+                    src={Instagram}
+                    alt="Instagram"
+                    style={{ width: "25px", height: "25px" }}
+                  />
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
